16d5238b59c4a00cfc05ce70a3bac6055f062623
Kevin Knights <kevinknights29@hotmail.com>
2023-10-26 12:42:12 -0500

    added pages and assets

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -8,6 +8,8 @@ UTILS_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_PACKAGE = os.path.dirname(UTILS_DIR)
 ROOT = os.path.dirname(PROJECT_PACKAGE)
 DATA_DIR = os.path.join(ROOT, "data")
+STYLES_FOLDER = os.path.join(ROOT, "styles")
+ASSETS_FOLDER = os.path.join(ROOT, "assets")
 
 # Files
 CONFIG_FILE = os.path.join(ROOT, "config.yaml")
